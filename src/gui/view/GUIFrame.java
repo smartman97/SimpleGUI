5fe69370d4a4cc4f35f0e13c19b3bd5ccd155152
Colm Laro <smartman97@gmail.com>
2015-11-09 09:39:44 -0700

    Added Button and TextField

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -1,8 +1,14 @@
 package gui.view;
 
 import gui.controller.GUIController;
-
 import javax.swing.JFrame;
+
+/**
+ * 
+ * @author Colm Laro
+ * @version 1.2 11/09/2015 Added a panel to the frame.
+ *
+ */
 
 public class GUIFrame extends JFrame
 {
@@ -20,7 +26,13 @@ public class GUIFrame extends JFrame
 	{
 		this.setContentPane(basePanel); //Must be the first line of setupFrame() -Installs panel in the frame.
 		this.setSize(400, 400);			//Fine a good size for the application.
+		this.setTitle("Simple GUI");
 		this.setResizable(false);		//Advisable not required.
 		this.setVisible(true);			//Must be the last line of setupFrame()
+	}
+	
+	public GUIController getBaseController()
+	{
+		return baseController;
 	}
 }
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,14 +1,38 @@
 package gui.view;
 
 import gui.controller.GUIController;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class GUIPanel extends JPanel
 {	
 	private GUIController baseController;
+	private JButton firstButton;
+	private JTextField firstTextField;
 	
 	public GUIPanel(GUIController baseController)
 	{
 		this.baseController = baseController;
+		
+		firstButton = new JButton("Press Button");
+		firstTextField = new JTextField("Type Here");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	private void setupListeners()
+	{
+		
+	}
+
+	private void setupLayout()
+	{
+		
+	}
+
+	private void setupPanel()
+	{
+		
 	}
 }
